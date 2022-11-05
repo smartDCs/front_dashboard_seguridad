adf6389f732ec697c8a460704a9aac398ff3d88c
Domotiz Arq <davidchicaisa@gmail.com>
2022-11-05 18:58:44 -0500

    Accediendo de forma remota a la raspberry pi

--- src/layouts/cctv/components/BillingInformation/index.js
+++ src/layouts/cctv/components/BillingInformation/index.js
@@ -2,15 +2,17 @@
 
 // @mui material components
 import Card from "@mui/material/Card";
-
+import React from 'react';
 // Soft UI Dashboard React components
 import SoftBox from "components/SoftBox";
 import SoftTypography from "components/SoftTypography";
 
-// Billing page components
-import Bill from "layouts/cctv/components/Bill";
+
+
 
 function BillingInformation() {
+ 
+ 
   return (
     <Card id="delete-account">
       <SoftBox pt={3} px={2}>
@@ -18,15 +20,11 @@ function BillingInformation() {
           Vistas en directo
         </SoftTypography>
       </SoftBox>
-      <SoftBox pt={0} pb={2} px={2}>
-        <SoftBox component="ul" display="flex" flexDirection="column" p={0} m={0}>
-        <video  width="750" height="500" controls autoPlay>
-        <source src="rtsp://admin:tesis2022@192.168.1.116:554/onvif1" type="application/x-rtsp"/>
-     </video>
-        
-         
-         
-        </SoftBox>
+      <SoftBox pt={0} pb={3} px={2}>
+      <iframe src=" http://proxy19.rt3.io:37413" width="100%" height="600" >
+
+</iframe>
+       
       </SoftBox>
     </Card>
   );
